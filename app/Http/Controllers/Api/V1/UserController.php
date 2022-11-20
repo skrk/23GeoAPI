21795d73da8539d6282fdbb9e6a4f6c89cbe03f3
Alex <skurko.a@ya.ru>
2022-11-20 20:11:09 +0300

    User and token generation

---
+++ app/Http/Controllers/Api/V1/UserController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Models\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function getToken(){
+        $credentials = [
+            'email' => 'admin@test.com',
+            'password' => '!password'
+        ];
+
+        if(!Auth::attempt($credentials)){
+            
+            $user = new User();
+            $user->name = 'admin';
+            $user->email = $credentials['email'];
+            $user->password = Hash::make($credentials['password']);
+            $user->save();
+
+            if(Auth::attempt($credentials)){
+                /** @var \App\Models\User $user **/
+                $user = Auth::user();
+                
+                $token = $user->createToken('token', ['create', 'update', 'delete']);
+
+                return [
+                    'token' => $token->plainTextToken
+                ];
+            }
+        }
+    }
+}
